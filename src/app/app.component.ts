5636a17eba040830c2026cce6b686efcfc45e694
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2025-08-21 03:21:15 +0100

    announce-sticker.component ✅

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import {HomeComponent} from './pages/home/home.component';
+
 
 @Component({
   selector: 'app-root',
   imports: [
-    HomeComponent
+    HomeComponent,
 
   ],
   templateUrl: './app.component.html',
