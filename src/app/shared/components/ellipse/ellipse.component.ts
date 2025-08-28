5fd5f767f39c7ca6b48f521db19c7f309bde81c1
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2025-08-28 03:26:33 +0100

    Background decoration ✅

--- src/app/shared/components/ellipse/ellipse.component.ts
+++ src/app/shared/components/ellipse/ellipse.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import {BgBlurComponent} from '../bg-blur/bg-blur.component';
 
 @Component({
   selector: 'app-ellipse',
-  imports: [
-    BgBlurComponent
-  ],
   templateUrl: './ellipse.component.html',
   styleUrl: './ellipse.component.css'
 })
