3ec006fba800d6d027330db901d5e2d8d21bc841
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2025-09-05 15:33:30 +0100

    adding loader✅

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
 import {HomeComponent} from './pages/home/home.component';
+import {LoaderComponent} from './shared/components/loader/loader.component';
+import {RouterOutlet} from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   imports: [
     HomeComponent,
+    LoaderComponent,
+    RouterOutlet,
 
   ],
   templateUrl: './app.component.html',
