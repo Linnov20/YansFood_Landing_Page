42fad3853cf67df8437cec35d421756eff608d0f
etie20 <etienne.foyang@2026.ucac-icam.com>
2025-10-01 09:06:22 +0100

    adding apk with drive

--- src/app/shared/components/secondary-button/secondary-button.component.ts
+++ src/app/shared/components/secondary-button/secondary-button.component.ts
@@ -16,7 +16,7 @@ export class SecondaryButtonComponent {
 
 
   handleDownload() {
-    window.location.href = "/yansfood-release.apk";
+    window.location.href = "https://drive.google.com/file/d/1r1rSHNhHNFcjecs-bVMG8f0Op7lETWBz/view?usp=sharing";
     this.downloadService.logDownload().subscribe({
       next: (res) => {
         console.log("Téléchargement comptabilisé :", res);
